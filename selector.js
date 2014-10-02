4f1254176542f30a25a65c38a2474c0d3a3e7bdb
vi <dev.wayne.ix@gmail.com>
2014-10-02 21:27:39 +0800

    selector

--- selector.js
+++ selector.js
@@ -1,10 +1,34 @@
 (function(global,doc){
-	function selector(select,context)
-		var reuslt=[];
-		if(select==null){
-			return reuslt;
+	var regAdjoin=/[#||.]\w/g;
+
+	function _split(selector){
+		var result=[];
+		if(selector==null){
+			return result;
 		}
-		
-		return reuslt;
+		selector = selector.replace(/\s+/g, ' ');
+		result=selector.split(" ");
+		return result;
 	}
+
+	function _adjoin(selector){
+		var result=[];
+		if(selector.lastIndexOf(".")>0||selector.lastIndexOf("#")>0){
+			result=selector.match(regAdjoin);
+		}
+		return result;
+	}
+
+	function selector(str){
+		var splitArr=[];
+		splitArr=_split(str);
+		if(splitArr.length>1){
+			splitArr.forEach(function(val,idx){
+				splitArr[idx]=_adjoin(val);
+				console.log(splitArr[idx]);
+			})
+		}
+		return splitArr;
+	}
+	 global.selector=selector;
 })(window,document)
